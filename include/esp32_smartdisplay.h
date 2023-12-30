5a8161cf23da906cc0cae4197c007f992db882e7
Rene Zeldenthuis <rzeldent@gmail.com>
2023-12-30 18:42:47 +0100

    3pt calibration

--- include/esp32_smartdisplay.h
+++ include/esp32_smartdisplay.h
@@ -7,8 +7,7 @@
 // LVGL lines buffered
 #define LVGL_PIXEL_BUFFER_LINES 16
 
-// Backlight PWM
-// Use last PWM_CHANNEL
+// Use last PWM_CHANNEL for backlight
 #define PWM_CHANNEL_BCKL (SOC_LEDC_CHANNEL_NUM - 1)
 #define PWM_FREQ_BCKL 5000
 #define PWM_BITS_BCKL 8
@@ -19,8 +18,25 @@
 extern "C"
 {
 #endif
+    // Structure to store the data from the three point calibration data
+    typedef struct
+    {
+        bool valid;
+        float alphaX;
+        float betaX;
+        float deltaX;
+        float alphaY;
+        float betaY;
+        float deltaY;
+    } touch_calibration_data_t;
+
     // Initialize the display and touch
     void smartdisplay_init();
+#ifdef BOARD_HAS_TOUCH
+    // Touch calibration
+    touch_calibration_data_t touch_calibration_data;
+    touch_calibration_data_t smartdisplay_compute_touch_calibration(const lv_point_t screen[3], const lv_point_t touch[3]);
+#endif    
     // Set the brightness of the backlight display
     void smartdisplay_lcd_set_backlight(float duty); // [0, 1]
     // Set the adaptive brightness callback
