0328ccee72633ce487d1f9b68fbf8d8b178ff755
Rene <rzeldent@users.noreply.github.com>
2024-03-20 08:17:08 +0100

    Swap xy and mirror not supported by build in / rom driver (#148)
    * Prepared new version 2.0.4
    
    * Updated boards
    
    * Updated platformio.ini
    
    * Added extern for touch_calibration_data
    
    * New build system for CI
    
    * Updated PCLK_HZ
    
    * UPdated variables for ST7789
    
    * Update Variables GC9A01
    
    * Updated ILI9341
    
    * LVGL to ^8.3.9
    
    * Typos
    
    * DST7262 variables
    
    * Typos and ST7701 variables
    
    * ST7701_LINE_CONFIG
    
    * ST7796 variables
    
    * SPI_DMA_CHANNEL
    
    * CST816S variables
    
    * GT911 variables
    
    * XPT2046 variables
    
    * Typo
    
    * esp_lcd_panel_io_3wire_spi_config_t
    
    * Updated boards
    
    * Updated boards
    
    * BOARD_HAS_
    
    * Update board
    
    * rename with interfaces
    
    * Added Interface type to boards for LCD/TOUCH
    
    * All boards
    
    * wip ESP32-24232s022
    
    * Updated 2432S022C
    
    * Updated boards
    Added warning for st7789_i80
    
    * Added 8048S070R
    
    * Updated boards
    
    * Do not invert colors
    
    * Added IPS Setting
    
    * rename, include strength
    
    * Removed v11 version
    
    * Added logging
    
    * logging
    
    * Removed touch_config flags
    
    * Removed not required backlight set
    
    * Added strength
    
    * GT911, fix for non matching touch resolutions
    
    * UPdated boards
    
    * Updated boards
    
    * GT911 vendorId
    
    * updated boards
    
    * Update board
    
    * Boards to main
    
    * Updated boards
    
    * UPdate boards
    
    * %d->%f
    
    * Added logging for vendor config initialization
    
    * Two decimals
    
    * Updated boards
    
    * Data bits are upper D8-D15
    
    * Updated boards
    
    * Updated boards
    
    * Added PlatformIo badge
    
    * Use LVGL pixel buffer size and malloc flags for json (#124)
    
    * Updated boards
    
    * Added spi_bus_config_t flags, intr flags and max_size
    
    * Small changes esp3228S022
    
    * Update version to 2.0.5
    
    * Minor changes to st7789_i80
    
    * BCKL to GPIO_BCKL
    
    * Typo miso_io_num
    
    * Changed location of tuning off the display when initializing
    
    * Updated boards
    
    * Updated boards
    
    * 21-nothing-shown-on-screen-only-backlight
    
    * Updated boards to current develop
    
    * Do not check for information GT911
    
    * Reenabled reading GTInfo: is required for resolution
    
    * Attempt to read GTInfo else use defaults
    
    * Updated boards
    
    * Updated boards + library version
    
    * Set PWM frtequency to 400Hz
    
    * New xpt2046 driver
    
    * Invert if IPS
    
    * Removed settings for XPT2046
    
    * Fixed typo ptr in logging
    Updated boards/platformio.ini
    
    * Work in progress GT911
    
    * Update for gt911
    
    * Corrected rot swap touch
    
    * Move esp_lcd_touch to library
    
    * Updated defines
    
    * Updated boards
    
    * Test for all boards
    
    * Flags not yet!
    
    * Added export/exclude
    
    * Updated glob
    
    * Updated exclude globs
    
    * Moved touch libraries back in esp32-smartdisplay
    
    * Updated touch
    
    * Work in progress gc9a01
    
    * Move addressed I2C to header
    
    * Added logging
    
    * Readded io_expander.c
    
    * Disabled reading info from gt911. Seems to cause problems
    
    * fixes
    
    * Typo refactor
    
    * calloc fix!
    
    * fixed for touch
    
    * Typo
    
    * Fixes
    
    * Fixes
    
    * fixes
    
    * fixes
    
    * st7796
    
    * parameter checking for touch
    
    * Fixes
    
    * Fixes
    
    * fixes
    
    * VendorConfig
    
    * Remove cmd/cmds_size
    
    * Fixed
    
    * Fixes
    
    * Test
    
    * Update boards
    
    * 7701
    
    * Fixes
    
    * fixes
    
    * Fixes rotation
    
    * lcd mirror conditional
    
    * TOUCH_SWAP_XY TOUCH_MIRROR_XY
    
    * All platforms
    
    * Bugfix for more points than configured
    
    * Updated MD/boards/platformio.ini
    
    * Updated boards
    
    * Updated boards
    
    * Updated boards
    
    * Updated MD
    
    * Display rotation not working (#145)
    
    * rotation test
    
    * Corrected rotation
    
    * LCD->DISPLAY
    
    * Updated boards
    
    * GAP_X/GAP_Y
    
    * All boards
    
    * Updated boards
    
    * Update to 2.0.9
    
    * Not supported on buildin driver

--- src/lvgl_panel_st7262_par.c
+++ src/lvgl_panel_st7262_par.c
@@ -76,7 +76,7 @@ void lvgl_lcd_init(lv_disp_drv_t *drv)
     ESP_ERROR_CHECK(esp_lcd_panel_swap_xy(panel_handle, DISPLAY_SWAP_XY));
 #endif
 #if defined(DISPLAY_MIRROR_X) || defined(DISPLAY_MIRROR_Y)    
-    ESP_ERROR_CHECK(esp_lcd_panel_mirror(panel_handle, DISPLAY_MIRROR_X, DISPLAY_MIRROR_Y));
+    //ESP_ERROR_CHECK(esp_lcd_panel_mirror(panel_handle, DISPLAY_MIRROR_X, DISPLAY_MIRROR_Y));
 #endif    
 #if defined(DISPLAY_GAP_X) || defined(DISPLAY_GAP_Y)
     ESP_ERROR_CHECK(esp_lcd_panel_set_gap(panel_handle, DISPLAY_GAP_X, DISPLAY_GAP_Y));
